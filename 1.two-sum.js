bdace1b1c63175692563a1d7e1ebc0640fcdf574
luyaj <1579618152@qq.com>
2019-12-24 10:11:04 +0800

    commit

---
+++ 1.two-sum.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+        if (nums[j] === target - nums[i]) {
+            return [i, j];
+        }
+    }
+  }
+};
+
